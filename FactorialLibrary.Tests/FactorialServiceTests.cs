6553af1fa00b04f454372b0cdd5a4d194478dd19
dngrwllrbnsn <145873152+dngrwllrbnsn@users.noreply.github.com>
2025-03-17 02:34:40 +0000

    added negative checks/exceptions

--- FactorialLibrary.Tests/FactorialServiceTests.cs
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -7,8 +7,10 @@ public class FactorialServiceTests
     [Fact]
     public void Test1()
     {
-        var result = FactorialService.Factorial(0);
-        Assert.Equal(1,result);
+        Assert.Throws<InvalidOperationException>(()=>{
+            var result = FactorialService.Factorial(-1);
+        });
+        
 
     }
 }
